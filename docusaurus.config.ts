7eb671f4df93e4be620c569837303fc5af21d35c
Kevin Manoj <kevinmanoj100@gmail.com>
2026-04-29 12:52:52 +0530

    Updated User Manual - Latest documentation with an added search bar

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -66,6 +66,19 @@ const config: Config = {
       },
     ],
   ],
+  themes: [
+    [
+      require.resolve('@easyops-cn/docusaurus-search-local'),
+      {
+        hashed: true,
+        language: ['en'],
+        indexDocs: true,
+        indexBlog: false,
+        indexPages: false,
+        docsRouteBasePath: ['docs', 'adk', 'connectors', 'tools'],
+      },
+    ],
+  ],
   themeConfig: {
     image: 'TODO',
     navbar: {
